38592671940a0b92a436578d608bec8d6e7283f4
al2b <deboissieu.al@gmail.com>
2018-06-23 19:46:51 +0200

    add compil train page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -53,6 +57,27 @@ class DefaultController extends Controller
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @return BinaryFileResponse
+     * @Route("compil-train/download", name="download-compiltrain")
+     * @Method({"GET", "POST"})
+     */
+
+    public function downloadAction(Request $request)
+    {
+
+        /**
+         * Generate file to upload
+         */
+
+        $path = $this->get('kernel')->getRootDir() . '/../public/music/compil-train.wav';
+
+        $response = new BinaryFileResponse($path);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT,'compil-train.wav');
+
+        return $response;
+    }
 
 
 }
